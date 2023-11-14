daef815320b94698e180c98bc925133dfa505a73
Deifilia Ajuma <107396188+dajuma@users.noreply.github.com>
2023-11-14 14:09:05 -0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
-* _printf - Printf function
+* _printf - The printf function
 * @format: format.
 * Return: Printed chars.
 */
@@ -54,7 +54,7 @@ int _printf(const char *format, ...)
 }
 
 /**
-* print_buffer - Prints the contents of the buffer if it exist
+* print_buffer - the function prints the contents of the buffer if it exist
 * @buffer: Array of chars
 * @buff_ind: Index at which to add next char, represents the length.
 */
